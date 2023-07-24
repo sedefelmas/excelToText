b0acc2602e31bd8bdc576e2e03784bcd652adcb8
Sedef Elmas <sedef.elmas@yupana.com.tr>
2023-07-24 03:08:45 -0700

    Başlangıç
    Hatalı hali

---
+++ ExcelToText/Form1.Designer.cs
@@ -0,0 +1,81 @@
+﻿
+namespace ExcelToText
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(748, 44);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Excel -> Text File";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Courier New", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(17, 71);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(743, 498);
+            this.label1.TabIndex = 2;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(770, 581);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+    }
+}
+
---
+++ ExcelToText/Form1.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace ExcelToText
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string cellValue = null;
+            bool stop = false;
+            StringBuilder line = new StringBuilder();
+            string textLine = "";
+            do
+            {
+                string excelFilePath = string.Empty;
+                openFileDialog1.InitialDirectory = Application.StartupPath;
+                openFileDialog1.FileName = "*.xlsx";
+                openFileDialog1.Filter = "xlsx files (*.xlsx)|*.xlsx|All files (*.*)|*.*";
+                if (openFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    excelFilePath = openFileDialog1.FileName;
+                    Excel.Application xlApp = new Excel.Application();
+                    //Excel.Workbook xlWorkbook = xlApp.Workbooks.Add();
+                    Excel.Workbook xlWorkbook = xlApp.Workbooks.Open(excelFilePath);
+                    Excel._Worksheet xlWorksheet = xlWorkbook.Sheets[1];
+                    Excel.Range xlRange = xlWorksheet.UsedRange;
+                    int rowCount = xlRange.Rows.Count;
+                    int colCount = xlRange.Columns.Count;
+                    string[] list = new string[colCount];
+                    //List<string[]> lists = new List<string[]> ();
+                    string[][] lists = new string[rowCount][];
+                    for (int i = 0; i <= rowCount - 1; i++)
+                    {
+                        lists[i] = list;
+                        for (int j = 0; j <= colCount - 1; j++)
+                        {
+                            if (xlRange.Cells[i + 1, j + 1] != null && xlRange.Cells[i + 1, j + 1].Value2 != null)
+                            {
+                                cellValue = xlRange.Cells[i + 1, j + 1].Value2;
+                                list[j] = cellValue.ToString();
+                                label1.Text += string.Format("{0,-7}", cellValue.ToString());
+                            }
+                        }
+                        lists[i] = list;
+                        label1.Text += "\r\n";
+                    }
+                    xlApp.Workbooks.Close();
+                    xlApp.Quit();
+
+                    xlRange = null;
+                    xlWorksheet = null;
+                    xlWorkbook = null;
+                    xlApp = null;
+                    stop = true;
+                    for (int i = 0; i <= rowCount - 1; i++)
+                    {
+                        list = lists[i];
+                        for (int j = 0; j <= colCount - 1; j++)
+                        {
+                            textLine += string.Format("{0,-20}", list[j]);
+                        }
+                        textLine += "\r\n";
+                    }
+                    string textFilePath = string.Empty;
+                    saveFileDialog1.InitialDirectory = Application.StartupPath;
+                    saveFileDialog1.FileName = "*.txt";
+                    saveFileDialog1.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+                    if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+                    {
+                        textFilePath = saveFileDialog1.FileName;
+                        StreamWriter sw = new StreamWriter(textFilePath);
+                        for (int i = 0; i <= rowCount - 1; i++)
+                        {
+                            list = lists[i];
+                            for (int j = 0; j <= colCount - 1; j++)
+                            {
+                                sw.WriteLine(string.Format("{0,10}", list[j]));
+                            }
+                            sw.WriteLine("\r\n");
+                        }
+                        sw.Close();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Dosya kaydedilemedi!", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+                else
+                {
+                    if (label1.Text == " ")
+                    {
+                        MessageBox.Show("Excel dosyasını seçmediniz!", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        this.button1_Click(sender, e);
+                        break;
+                    }
+
+                }
+            }
+            while (stop == false);
+        }
+
+    }
+}
